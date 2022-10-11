05e51ff9add86aa19d16fdccfcb679a42a56f096
Daniel <s3786266@student.rmit.edu.au>
2022-10-11 20:48:56 +1100

    removed the additional classes

--- ms-accounts/src/main/java/com/rmit/sept/msaccounts/exception/AccountNumberException.java
+++ ms-accounts/src/main/java/com/rmit/sept/msaccounts/exception/AccountNumberException.java
@@ -2,12 +2,3 @@ package com.rmit.sept.msaccounts.exception;
 
 public class AccountNumberException extends RuntimeException  {
 
-    private  static final long serialVersionUID = 342423523;
-
-    private Integer value;
-
-    public AccountNumberException(String message, Integer value) {
-        super(message);
-        this.value = value;
-    }
-}
