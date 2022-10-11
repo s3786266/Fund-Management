4bfad8cfda65817b03627561e9a706a81a75f537
Daniel <s3786266@student.rmit.edu.au>
2022-10-11 21:31:12 +1100

    removed additional classes

--- ms-accounts/src/main/java/com/rmit/sept/msaccounts/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rmit.sept.msaccounts.repository;
-
-import com.rmit.sept.msaccounts.model.AccountDto;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountRepository  extends CrudRepository<AccountDto, Integer> {
-}
--- ms-accounts/src/main/java/com/rmit/sept/msaccounts/service/AccountService.java
+++
@@ -1,5 +0,0 @@
-package com.rmit.sept.msaccounts.service;
-
-public class AccountService {
-
-}
