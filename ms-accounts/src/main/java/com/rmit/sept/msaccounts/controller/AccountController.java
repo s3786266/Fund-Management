2fdf805d3fa206960891dd801363c35c4186c85f
Daniel <s3786266@student.rmit.edu.au>
2022-10-11 22:45:02 +1100

    Revert "removed the additional classes"
    This reverts commit 7206cbab15ed21288f4c13f3d4a8702a40c5b522.

--- ms-accounts/src/main/java/com/rmit/sept/msaccounts/controller/AccountController.java
+++ ms-accounts/src/main/java/com/rmit/sept/msaccounts/controller/AccountController.java
@@ -1,8 +1,43 @@
 package com.rmit.sept.msaccounts.controller;
 
+import com.rmit.sept.msaccounts.model.AccountDto;
+import com.rmit.sept.msaccounts.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class AccountController {
 
+    @Autowired
+    private AccountService accountService;
+
+    @PostMapping(path="/account/register")
+    public ResponseEntity<AccountDto> registerNewAccount(@RequestBody AccountDto accountDetails) {
+        return new ResponseEntity<>(accountService.saveNewAccountDetails(accountDetails), HttpStatus.CREATED);
+    }
+
+    @GetMapping(path="/accounts/account")
+    public List<AccountDto> getAllAccountDetails() {
+        return accountService.getAllAccountDetails();
+    }
+
+    @GetMapping(path="/account/{accountNumber}")
+    public Optional<AccountDto> getAccountDetailsByAccNo(@PathVariable Integer accountNumber) {
+        return accountService.getAccountDetailsByAccNo(accountNumber);
+    }
+
+    @PutMapping(path="/account/{accountNumber}")
+    public ResponseEntity<AccountDto> updateExistingAccountDetails(@PathVariable Integer accountNumber, @RequestBody AccountDto newAccountDetails) throws Exception {
+        return new ResponseEntity<>(accountService.updateExistingAccountDetails(accountNumber, newAccountDetails), HttpStatus.OK);
+    }
+
+    @DeleteMapping(path="/account/{accountNumber}")
+    public void deleteExistingAccountDetails(@PathVariable Integer accountNumber) {
+        accountService.deleteExistingAccountDetails(accountNumber);
+    }
 }
