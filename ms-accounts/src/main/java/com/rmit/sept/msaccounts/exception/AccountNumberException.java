9e96ed8d56ff5427c24b3e499eb34db3e7731e62
Daniel <s3786266@student.rmit.edu.au>
2022-10-11 22:47:32 +1100

    Revert "removed the additional classes"
    This reverts commit db4bb2aaa5b9a0fdc462db3a700db65b45b4b102.

--- ms-accounts/src/main/java/com/rmit/sept/msaccounts/exception/AccountNumberException.java
+++ ms-accounts/src/main/java/com/rmit/sept/msaccounts/exception/AccountNumberException.java
@@ -1,4 +1,13 @@
 package com.rmit.sept.msaccounts.exception;
 
-public class AccountNumberException extends RuntimeException {
+public class AccountNumberException extends RuntimeException  {
+
+    private  static final long serialVersionUID = 342423523;
+
+    private Integer value;
+
+    public AccountNumberException(String message, Integer value) {
+        super(message);
+        this.value = value;
+    }
 }
