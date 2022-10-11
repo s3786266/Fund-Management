33553b05decf6e9ddf976423be4e9759f08d8c66
Daniel <s3786266@student.rmit.edu.au>
2022-10-11 21:33:49 +1100

    added back the classes

--- ms-persons/src/main/java/com/rmit/sept/mspersons/exception/PersonIdException.java
+++ ms-persons/src/main/java/com/rmit/sept/mspersons/exception/PersonIdException.java
@@ -2,4 +2,12 @@ package com.rmit.sept.mspersons.exception;
 
 public class PersonIdException extends RuntimeException{
 
+    private  static final long serialVersionUID = 342423523;
+
+    private Integer value;
+
+    public PersonIdException(String message, Integer value) {
+        super(message);
+        this.value = value;
+    }
 }
